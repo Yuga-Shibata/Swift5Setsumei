0e0a924c2bbf976f8219fe27bed59eed09fbf64e
Yuga-Shibata <kazutodaze3@gmail.com>
2020-06-03 19:43:53 +0900

    Initial Commit

---
+++ Swift5SetsumeiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5SetsumeiApp
+//
+//  Created by 柴田優河 on 2020/06/03.
+//  Copyright © 2020 Makoto Shibata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
